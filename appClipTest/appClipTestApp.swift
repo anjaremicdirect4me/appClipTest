ed22fd4883acbf8db4154faf36d2c05fe23b6bc8
Anja Remic <anchyr@gmail.com>
2023-10-28 12:48:09 +0200

    Initial Commit

---
+++ appClipTest/appClipTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  appClipTestApp.swift
+//  appClipTest
+//
+//  Created by Anja Remic on 28. 10. 23.
+//
+
+import SwiftUI
+
+@main
+struct appClipTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
